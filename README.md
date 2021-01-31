fa635db16cf15fb32e907e4e98f52e9f0d44913c
ychan2345 <ychan2345@gmail.com>
2021-01-31 16:22:46 -0500

    Uploaded new files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # streatmlit_deploy_test
+Simple streamlit application
+
+# Required Files
+1 setup.sh
+2 Procfile
+3 requirements.txt
---
+++ app.py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+import pandas as pd
+
+def main():
+
+	""" A simple streamlit application """
+
+	st.title("A test of the streamlit application")
+
+	st.subheader("The app is working")
+
+
+if __name__ == '__main__':
+	main()
+
+

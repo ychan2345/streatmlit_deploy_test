28a8566f412e1fee9a00c84d6040d703912e0b7d
ychan2345 <ychan2345@gmail.com>
2024-07-16 18:19:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 
 import streamlit as st
 
-def main():
+st.title("A test of the streamlit application")
 
-	""" A simple streamlit application """
-
-	st.title("A test of the streamlit application")
-
-	st.subheader("Streamlit app works")
-
-
-if __name__ == '__main__':
-	main()
+st.subheader("Streamlit app works")
 
 
